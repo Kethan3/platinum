59d9801e32c379df4e7e46e9040ac01a442b2c9e
Kethan3 <kethangowda003@gmail.com>
2025-05-05 10:23:55 +0530

    initialize project with essential configuration files and setup

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const allRoutes = new Hono();
+
+allRoutes.get("", (context) => {
+  return context.json({
+    message: "hello world",
+  });
+});
+
+serve(allRoutes, ({ port }) => {
+  console.log(`running at http://localhost${port}`);
+});
