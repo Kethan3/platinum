a34ceebbcc4cb097f2d0ec2cf38da259ae5e2a22
Kethan3 <kethangowda003@gmail.com>
2025-05-06 10:32:46 +0530

    add Post model and migration; implement session middleware for authentication

--- src/routes/authentications/index.ts
+++ src/routes/authentications/index.ts
@@ -1,8 +1,9 @@
-import { Hono } from "hono";
+
 import { betterAuthClient } from "../../integration/better-auth";
+import { createUnsecureRoute } from "../middlewares/session-middleware";
 
 
-export const authenticationsRoute = new Hono();
+export const authenticationsRoute =  createUnsecureRoute();
 
 authenticationsRoute.use((context) => {
   return betterAuthClient.handler(context.req.raw);
